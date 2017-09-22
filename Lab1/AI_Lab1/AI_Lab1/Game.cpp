f2674fbfcf99b257ee61e2b335b568d888b53c63
JohnMcGrath <mcgrathjohnjerome@gmail.com>
2017-09-22 09:57:39 +0100

    Lab 1 COmplete

---
+++ Lab1/AI_Lab1/AI_Lab1/Game.cpp
@@ -0,0 +1,175 @@
+// author Peter Lowe
+
+#include "Game.h"
+#include <iostream>
+
+
+
+Game::Game() :
+	m_window{ sf::VideoMode{ 800, 600, 32 }, "SFML Game" },
+	m_exitGame{false} //when true game will exit
+{
+	setupFontAndText(); // load font 
+	setupSprite(); // load texture
+}
+
+
+Game::~Game()
+{
+}
+
+
+void Game::run()
+{
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	sf::Time timePerFrame = sf::seconds(1.f / 60.f); // 60 fps
+	while (m_window.isOpen())
+	{
+		processEvents(); // as many as possible
+		timeSinceLastUpdate += clock.restart();
+		while (timeSinceLastUpdate > timePerFrame)
+		{
+			timeSinceLastUpdate -= timePerFrame;
+			processEvents(); // at least 60 fps
+			update(timePerFrame); //60 fps
+		}
+		render(); // as many as possible
+	}
+}
+/// <summary>
+/// handle user and system events/ input
+/// get key presses/ mouse moves etc. from OS
+/// and user :: Don't do game update here
+/// </summary>
+void Game::processEvents()
+{
+	sf::Event event;
+	while (m_window.pollEvent(event))
+	{
+		if ( sf::Event::Closed == event.type) // window message
+		{
+			m_window.close();
+		}
+		if (sf::Event::KeyPressed == event.type) //user key press
+		{
+			if (sf::Keyboard::Escape == event.key.code)
+			{
+				m_exitGame = true;
+			}
+			if (sf::Keyboard::Up == event.key.code)
+			{
+				rectVelocityY--;
+			}
+			if (sf::Keyboard::Down == event.key.code)
+			{
+				rectVelocityY++;
+			}
+		}
+	}
+}
+
+/// <summary>
+/// Update the game world
+/// </summary>
+/// <param name="t_deltaTime">time interval per frame</param>
+void Game::update(sf::Time t_deltaTime)
+{
+	rect.setPosition(sf::Vector2f(rect.getPosition().x + rectVelocityX, rect.getPosition().y + rectVelocityY));
+	circ.setPosition(sf::Vector2f(circ.getPosition().x + circVelocityX, circ.getPosition().y + circVelocityY));
+
+	if (circ.getPosition().x > m_window.getSize().x)
+	{
+		circ.setPosition(sf::Vector2f(0 - circ.getGlobalBounds().width, circ.getPosition().y));
+	}
+
+	if (circ.getPosition().y > m_window.getSize().y)
+	{
+		circ.setPosition(sf::Vector2f(circ.getPosition().x, 0 - circ.getGlobalBounds().height));
+	}
+
+
+
+	if (rect.getPosition().x > m_window.getSize().x)
+	{
+		rect.setPosition(sf::Vector2f(0, rect.getPosition().y));
+		std::cout << "TTTTTTT" << std::endl;
+	}
+	
+	if (rect.getPosition().x < 0)
+	{
+		rect.setPosition(sf::Vector2f(m_window.getSize().x, rect.getPosition().y));
+		std::cout << "TTTTTTT" << std::endl;
+	}
+
+	if (rect.getPosition().y > m_window.getSize().y)
+	{
+		rect.setPosition(sf::Vector2f(rect.getPosition().x, 0));
+		std::cout << "Reset to top" << std::endl;
+	}
+	if (rect.getPosition().y < 0)
+	{
+		rect.setPosition(sf::Vector2f(rect.getPosition().y, m_window.getSize().y));
+		std::cout << "TTTTTTT" << std::endl;
+	}
+
+	if (m_exitGame)
+	{
+		m_window.close();
+	}
+}
+
+/// <summary>
+/// draw the frame and then switch bufers
+/// </summary>
+void Game::render()
+{
+	m_window.clear(sf::Color::White);
+	m_window.draw(m_welcomeMessage);
+	m_window.draw(m_logoSprite);
+	m_window.draw(circ);
+	m_window.draw(rect);
+	m_window.display();
+}
+
+/// <summary>
+/// load the font and setup the text message for screen
+/// </summary>
+void Game::setupFontAndText()
+{
+	if (!m_ArialBlackfont.loadFromFile("ASSETS\\FONTS\\ariblk.ttf"))
+	{
+		std::cout << "problem loading arial black font" << std::endl;
+	}
+	m_welcomeMessage.setFont(m_ArialBlackfont);
+	m_welcomeMessage.setString("SFML Game");
+	m_welcomeMessage.setStyle(sf::Text::Underlined | sf::Text::Italic | sf::Text::Bold);
+	m_welcomeMessage.setPosition(40.0f, 40.0f);
+	m_welcomeMessage.setCharacterSize(80);
+	m_welcomeMessage.setOutlineColor(sf::Color::Red);
+	m_welcomeMessage.setFillColor(sf::Color::Black);
+	m_welcomeMessage.setOutlineThickness(3.0f);
+
+}
+
+/// <summary>
+/// load the texture and setup the sprite for the logo
+/// </summary>
+void Game::setupSprite()
+{
+	if (!m_logoTexture.loadFromFile("ASSETS\\IMAGES\\SFML-LOGO.png"))
+	{
+		// simple error message if previous call fails
+		std::cout << "problem loading logo" << std::endl;
+	}
+	m_logoSprite.setTexture(m_logoTexture);
+	m_logoSprite.setPosition(300.0f, 180.0f);
+
+	rect.setFillColor(sf::Color::Red);
+	rect.setSize(sf::Vector2f(50, 50));
+	rect.setPosition(30, 30);
+
+	circ.setFillColor(sf::Color::Blue);
+	circ.setRadius(25);
+	circ.setPosition(100, 30);
+}
---
+++ Lab1/AI_Lab1/AI_Lab1/Game.h
@@ -0,0 +1,43 @@
+// author Peter Lowe
+#ifndef GAME
+#define GAME
+
+#include <SFML/Graphics.hpp>
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	/// <summary>
+	/// main method for game
+	/// </summary>
+	void run();
+
+private:
+
+	void processEvents();
+	void update(sf::Time t_deltaTime);
+	void render();
+	
+	void setupFontAndText();
+	void setupSprite();
+
+	sf::RenderWindow m_window; // main SFML window
+	sf::Font m_ArialBlackfont; // font used by message
+	sf::Text m_welcomeMessage; // text used for message on screen
+	sf::Texture m_logoTexture; // texture used for sfml logo
+	sf::Sprite m_logoSprite; // sprite used for sfml logo
+	bool m_exitGame; // control exiting game
+
+	sf::RectangleShape rect;
+	int rectVelocityX = 0;
+	int rectVelocityY = 3;
+
+	sf::CircleShape circ;
+	int circVelocityX = 3;
+	int circVelocityY = 0;
+};
+
+#endif // !GAME
+
